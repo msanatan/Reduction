9eda32d4c0822e77b3ec630184f70f33b9a72e63
Marcus Sanatan <msanatan@gmail.com>
2022-04-18 09:46:45 -0400

    Smoother grid movement
    We somewhat slowly move the player to the posiition, and snap if we overshoot.
    It's not as rough as just adding one, but cool

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    //void Start()
-    //{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float snapDistance = 1f;
+    Vector3 targetPosition;
+    Vector3 startPosition;
+    bool moving = false;
 
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (moving)
+        {
+            if (Vector3.Distance(startPosition, transform.position) > snapDistance)
+            {
+                transform.position = targetPosition;
+                moving = false;
+                return;
+            }
+
+            transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left;
+            targetPosition = transform.position + Vector3.left;
+            startPosition = transform.position;
+            moving = true;
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right;
+            targetPosition = transform.position + Vector3.right;
+            startPosition = transform.position;
+            moving = true;
         }
         else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.position += Vector3.back;
+            targetPosition = transform.position + Vector3.back;
+            startPosition = transform.position;
+            moving = true;
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            transform.position += Vector3.forward;
+            targetPosition = transform.position + Vector3.forward;
+            startPosition = transform.position;
+            moving = true;
         }
     }
 }
